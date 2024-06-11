6e522f63b16e7137ccf7f1f3e5a479df2fed169b
Nikita-Vesel0v <nikiveselo2004@gmail.com>
2024-06-11 17:47:53 +0300

    first commit. example working

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+package bullscows
+
+fun main() {
+    println("Hello World! It's Bulls and Cows")
+
+    println("""
+        The secret code is prepared: ****.
+
+        Turn 1. Answer:
+        1234
+        Grade: None.
+
+        Turn 2. Answer:
+        9876
+        Grade: 4 bulls.
+        Congrats! The secret code is 9876.
+    """.trimIndent())
+
+}
